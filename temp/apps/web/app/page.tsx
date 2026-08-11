71249a50f055d3bc7b66d5a84767f67540f5d295
Leonardo <leonardorogerio7@icloud.com>
2026-08-11 14:35:14 -0300

    feat: add animated heartbeat and refactor dashboard layout

--- temp/apps/web/app/page.tsx
+++ temp/apps/web/app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import Header from "../components/layout/Header";
 import Dashboard from "../components/dashboard/Dashboard";
+import MissionCockpit from "../components/cockpit/MissionCockpit";
+import Timeline from "../components/timeline/Timeline";
 
 export default function Home() {
   return (
