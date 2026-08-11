6f98598ce2935c2d54a6ec811a5e3b20615d618a
Leonardo <leonardorogerio7@icloud.com>
2026-08-11 14:22:31 -0300

    feat: add animated heartbeat and refactor dashboard layout

--- temp/apps/web/app/page.tsx
+++ temp/apps/web/app/page.tsx
@@ -1,46 +1,13 @@
 "use client";
 
-import Header from "../components/layout/Header";
-import StatusCard from "../components/cards/StatusCard";
-import MissionCockpit from "../components/cockpit/MissionCockpit";
-import Timeline from "../components/timeline/Timeline";
+import Dashboard from "../components/dashboard/Dashboard";
 
 export default function Home() {
   return (
     <>
       <Header />
 
-      <div
-        style={{
-          display: "flex",
-          gap: 20,
-          marginTop: 30,
-        }}
-      >
-        <StatusCard
-          icon="❤️"
-          title="Heartbeat"
-          value="ONLINE"
-        />
-
-        <StatusCard
-          icon="⚽"
-          title="Mercados"
-          value="0"
-        />
-
-        <StatusCard
-          icon="🎯"
-          title="Missões"
-          value="0"
-        />
-
-        <StatusCard
-          icon="🛰️"
-          title="Sentinel"
-          value="Inicializando..."
-        />
-      </div>
+      <Dashboard />
 
       <MissionCockpit />
 
